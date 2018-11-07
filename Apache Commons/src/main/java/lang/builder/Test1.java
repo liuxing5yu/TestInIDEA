7023d85534f68c7aa6b4a2e8abfb9d51a1408595
Wenjie Huang <liuxing5yu@users.noreply.github.com>
2018-11-07 17:30:08 +0800

    开始测试Apache Commons

---
+++ Apache Commons/src/main/java/lang/builder/Test1.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package lang.builder;
+
+import org.apache.commons.lang3.builder.CompareToBuilderTest;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * @author hwj
+ * @date 2018年5月16日 下午4:33:49
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		CompareToBuilderTest test = new CompareToBuilderTest();
+		test.testReflectionCompare();
+	}
+}

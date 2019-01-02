f9e201c1ad2cf36b824191ebd4f9929de509be28
Wenjie Huang <liuxing5yu@users.noreply.github.com>
2019-01-02 20:47:31 +0800

    DOJO例子

---
+++ DOJO/html/6/dtdg/psychic/nls/es/reading.js
@@ -0,0 +1,7 @@
+{
+    /* ... */
+    reading101 : "¿Eres un Libra, no, mi corazón?",
+        reading102: "¿Me puedes dar el nombre y tu cumpleaños por favor?",
+    reading103: "Sí, el es papá"
+    /* ... */
+}
---
+++ DOJO/html/6/dtdg/psychic/nls/reading.js
@@ -0,0 +1,7 @@
+{
+    /* ... */
+    reading101 : "You're a Libra, aren't ya darling?",
+        reading102: "Can you please tell me your first name only, and your birthday please?",
+    reading103: "Yep, that's the Daddy."
+    /* ... */
+}

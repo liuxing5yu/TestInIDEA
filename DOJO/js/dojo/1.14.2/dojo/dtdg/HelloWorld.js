c0847dc808f7d011b57b97c250920d56aced0d72
hwj <809534335@qq.com>
2019-01-08 22:04:22 +0800

    Dojo例子

--- DOJO/js/dojo/1.14.2/dojo/dtdg/HelloWorld.js
+++ DOJO/js/dojo/1.14.2/dojo/dtdg/HelloWorld.js
@@ -1,29 +1,28 @@
-//Summary: An example HelloWorld dijit that illustrates Dojo's basic dijit
-//design pattern
-
-//The first line of any module file should have exactly one dojo.provide
-//specifying the resource and any membership in parent modules. The name
-//of the resource should be the same as the .js file.
 dojo.provide("dtdg.HelloWorld");
 
-//Always require resources before you try to use them. We're requiring these
-//two resources because they're part of our dijit's inheritance hierarchy.
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 
-//The feature rich constructor that allows us to declare Dojo "classes".
 dojo.declare(
     "dtdg.HelloWorld",
-
-    //dijit._Widget is the prototypical ancestor that provides important method
-    //stubs like the ones below.
-    //dijit._Templated is then mixed in and overrides dijit._Widget's
-    //buildRendering method, which constructs the UI for the dijit from
-    //a template.
     [dijit._Widget, dijit._Templated],
     {
-        //Path to the template of this dijit. dijit._Templated uses this to
-        //snatch the template from the named file via a synchronous call.
-        templatePath: dojo.moduleUrl("dtdg", "templates/HelloWorld.html")
+        templateString:
+            "<span class='hello_class' dojoAttachEvent='onmouseover:onMouseOver, onmouseout: onMouseOut'>" +
+            "Hello World</span>",
+
+        onMouseOver: function (evt) {
+            dojo.addClass(this.domNode, 'hello_class');
+            console.log("applied hello_class...");
+            console.log(evt);
+        },
+
+        onMouseOut: function (evt) {
+            dojo.removeClass(this.domNode, 'hello_class');
+            console.log("removed hello_class...");
+            console.log(evt);
+        }
+
     }
-);
+)
+;
